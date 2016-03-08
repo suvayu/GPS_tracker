815680614d021a1753ec21aa99b90d2cddd4f4ed
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2016-03-08 17:10:53 +0530

    Directory local variables for Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode
+  (flycheck-disabled-checkers . (c/c++-clang))
+  (flycheck-c/c++-gcc-executable . "avr-gcc")
+  (flycheck-gcc-include-path . ("."))))
